70b9e712a7fd260e8491597e4191d0b543f55459
MichiTheGenius <litzinger04@gmail.com>
2022-02-03 19:01:40 +0100

    added settings.py file with rgb colors and screen attributes

---
+++ settings.py
@@ -0,0 +1,11 @@
+# rgb colors
+WHITE = (0, 0, 0)
+BLACK = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# screen setup
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+FPS = 60
